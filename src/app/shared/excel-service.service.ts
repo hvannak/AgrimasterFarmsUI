1fc882ba23c6b69de7832c854251b38b82422fcc
hvannak <vannak2010@gmail.com>
2021-06-08 09:34:59 +0700

    add exceljs save file

---
+++ src/app/shared/excel-service.service.ts
@@ -0,0 +1,19 @@
+import { DatePipe } from '@angular/common';
+import { Injectable } from '@angular/core';
+import * as FileSaver from 'file-saver';
+
+const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+const EXCEL_EXTENSION = '.xlsx';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExcelServiceService {
+
+  constructor() { }
+
+  public saveAsExcelFile(buffer: any, fileName: string): void {
+    const data: Blob = new Blob([buffer], { type: EXCEL_TYPE });
+    FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+  }
+}
--- src/app/vaccine-details/vaccine-details.component.ts
+++ src/app/vaccine-details/vaccine-details.component.ts
@@ -8,6 +8,10 @@ import {map} from 'rxjs/operators';
 import { FormControl, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { VaccineImageComponent } from '../vaccine-image/vaccine-image.component';
+import { ExcelServiceService } from '../shared/excel-service.service';
+import { Workbook } from 'exceljs';
+import { formatDate } from '@angular/common';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-vaccine-details',
@@ -22,11 +26,11 @@ export class VaccineDetailsComponent implements OnInit {
   showFiller = false;
   resultsLength = 0;
   isLoadingResults = true;
-  pageSize = 10;
-  pageSizeOptions: number[] = [5, 10, 25, 100];
+  pageSize = 100;
+  pageSizeOptions: number[] = [100,200,500];
   projectID = new FormControl('', Validators.required);
 
-  constructor(public service:VaccineServiceService, 
+  constructor(public service:VaccineServiceService, public serviceExcel:ExcelServiceService,
     private toastr:ToastrService,public dialog: MatDialog) { 
       this.paginator = new MatPaginator(new MatPaginatorIntl(), ChangeDetectorRef.prototype);
       this.sort = new MatSort();
@@ -54,6 +58,58 @@ export class VaccineDetailsComponent implements OnInit {
     };
     console.log(pageOpt);
     this.getVaccineSchedulePage(pageOpt);
+  }
+
+  exportExcelFile(){
+    if(this.projectID.value){
+      const title = 'Vaccine Of Project ' + this.projectID.value;
+      const header = ["No","ProjectID","VacDate","Inventory","Qty","Ejected Number","Description","Status"];
+      const sheetname = "Vaccine";
+      const filename = "vaccine";
+      let workbook = new Workbook();
+      let worksheet = workbook.addWorksheet(sheetname);
+      worksheet.pageSetup.orientation = 'portrait';
+      let titleRow = worksheet.addRow([title]);
+      worksheet.addRow([]);
+      let headerRow = worksheet.addRow(header);
+      headerRow.eachCell((cell, number) => {
+        cell.fill = {
+          type: 'pattern',
+          pattern: 'solid',
+          fgColor: { argb: 'FFFFFF00' },
+          bgColor: { argb: 'FF0000FF' }
+        }
+        cell.border = { top: { style: 'thin' }, left: { style: 'thin' }, bottom: { style: 'thin' }, right: { style: 'thin' } }
+      });
+
+      let rownumber: number = 0;
+      let totalSalaryAmount:number = 0;
+      // let sortDataList = this.service.vaccineScheduleList.sortData(this.service.vaccineScheduleList.filteredData,this.service.vaccineScheduleList.sort);
+      this.service.vaccineScheduleList.data.forEach(element => {
+        rownumber = rownumber + 1;
+        let status = (element.StatusID == 0) ? 'Active' : 'Done';
+        let proDate = formatDate(element.VacDate, environment.format, environment.locale); 
+        let row = worksheet.addRow([rownumber, element.ProjectID, proDate, element.Inventory,
+          element.Qty, element.ChickEjected, element.Description,status]);
+        row.eachCell((cell,number) => {
+          cell.border = { top: { style: 'thin' }, left: { style: 'thin' }, bottom: { style: 'thin' }, right: { style: 'thin' } }
+        });
+      });
+
+      worksheet.getColumn(1).width = 5;
+      worksheet.getColumn(2).width = 20;
+      worksheet.getColumn(3).width = 20;
+      worksheet.getColumn(4).width = 20;
+      worksheet.getColumn(5).width = 10;
+      worksheet.getColumn(6).width = 10;
+      worksheet.getColumn(7).width = 20;
+      worksheet.getColumn(8).width = 10;
+      //Generate Excel File with given name
+      workbook.xlsx.writeBuffer().then((data) => {
+        let blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+        this.serviceExcel.saveAsExcelFile(blob, filename)
+      })
+    }
   }
 
   getVaccineSchedulePage(pageOpt:any){
